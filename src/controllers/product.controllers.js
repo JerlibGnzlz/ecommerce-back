2798491e575185408bf6b4d513c424316c5dc6a5
AgusBalda <agustibaldassari@gmail.com>
2022-06-23 09:22:41 -0300

    filter category and brand

--- src/controllers/product.controllers.js
+++ src/controllers/product.controllers.js
@@ -3,16 +3,69 @@ const {Op} = require('sequelize')
 const controller = {}
 
 controller.product = async (req, res) => {
-    
-    try{
-        res.status(200).send(await Product.findAll({
-            include: [
-                {model: Brand}, 
-                {model: Category}]
-        }))
-    }catch(err){
-        res.status(400).send(err)
+    let {category, brand} = req.body[0];
+    console.log(req.body[0])
+    if(category || brand){
+        if(category && brand){
+            try{
+                res.status(200).send(await Product.findAll({
+                    where:{
+                        [Op.and]:{
+                            brandId: brand,
+                            categoryId: category
+                        }
+                    },
+                    include: [
+                        {model: Brand}, 
+                        {model: Category}]
+                }))
+            }catch(err){
+                 res.status(400).send(err)
+            }
+        }
+
+        else if (category){
+            try{
+                res.status(200).send(await Product.findAll({
+                    where:{
+                        categoryId: category
+                    },
+                    include: [
+                        {model: Brand}, 
+                        {model: Category}]
+                }))
+            }catch(err){
+                 res.status(400).send(err)
+            }
+        }
+
+        else if(brand){
+            try{
+                res.status(200).send(await Product.findAll({
+                    where:{
+                        brandId: brand
+                    },
+                    include: [
+                        {model: Brand}, 
+                        {model: Category}]
+                }))
+            }catch(err){
+                res.status(400).send(err)
+            }
+        }
     }
+    
+    else{
+        try{
+            res.status(200).send(await Product.findAll({
+                include: [
+                    {model: Brand}, 
+                    {model: Category}]
+            }))
+        }catch(err){
+            res.status(400).send(err)
+        }
+    }   
 }
 
 module.exports = controller
