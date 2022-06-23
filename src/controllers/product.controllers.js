5af80e6b9c27e43ef34bea7f00b5f96bb0b99232
AgusBalda <agustibaldassari@gmail.com>
2022-06-23 08:44:47 -0300

    get all product incluide brand and category

--- src/controllers/product.controllers.js
+++ src/controllers/product.controllers.js
@@ -1,11 +1,14 @@
-const {Product} = require("../db");
+const {Product, Brand, Category} = require("../db");
 const {Op} = require('sequelize')
 const controller = {}
 
 controller.product = async (req, res) => {
     
     try{
-        res.status(200).send(await Product.findAll())
+        res.status(200).send(await Product.findAll({
+            include: Brand,
+            include: Category
+        }))
     }catch(err){
         res.status(400).send(err)
     }
