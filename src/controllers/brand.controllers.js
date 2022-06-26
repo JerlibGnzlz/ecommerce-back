34b6fa66fed34fb111f1e828df6ccd845d6a8b96
AgusBalda <agustibaldassari@gmail.com>
2022-06-26 11:51:34 -0300

    filtrado de categorias y marcas

--- src/controllers/brand.controllers.js
+++ src/controllers/brand.controllers.js
@@ -1,18 +1,60 @@
-const { Brand } = require("../db");
-//const {Op} = require('sequelize')
+const { Brand, Product } = require("../db");
+const {Op} = require('sequelize');
 const controller = {};
 
 controller.brand = async (req, res) => {
-  let { util } = req.query;
-  const allBrands = await Brand.findAll({});
+  let { genre, category } = req.query;
+  try{
+    let allBrands = undefined
+    let product = []
+    if(category){
+      product = await Product.findAll({
+        where: {
+          [Op.and]: {
+              genre: genre,
+              categoryId: category,
+          }
+        },
+    })}
+    else if (genre){
+      product = await Product.findAll({
+        where:{
+          genre:genre
+        }
+      })
+    }
+    else{
+      allBrands = await Brand.findAll()
+    }
 
-  if (!util) {
-    try {
-      res.status(200).send(allBrands);
-    } catch (err) {
-      res.status(400).send(err);
+    let arrayId = []
+    product.forEach(p => {
+      if (arrayId.includes(p.brandId)){
+        return
+      }
+      else{
+        arrayId.push(p.brandId)
+      }
+    })
+
+    if(arrayId.length !== 0){
+      res.status(200).send(await Brand.findAll({
+        where: {
+          id: arrayId
+        }
+      }));
+    } 
+    else if (allBrands){
+      res.status(200).send(allBrands)
+    }
+    else{
+      res.status(200).send("no hay marcas")
     }
   }
+  catch(err){
+    res.status(400).send(err)
+  }
+  
 };
 
 module.exports = controller;
--- src/controllers/category.controllers.js
+++ src/controllers/category.controllers.js
@@ -1,17 +1,58 @@
-const { Category } = require("../db");
-//const {Op} = require('sequelize')
+const { Category, Product } = require("../db");
+const {Op} = require('sequelize');
 const controller = {};
 
 controller.category = async (req, res) => {
-  let { util } = req.query;
-  const allCategories = await Category.findAll({});
-
-  if (!util) {
-    try {
-      res.status(200).send(allCategories);
-    } catch (err) {
-      res.status(400).send(err);
+  let { genre, brand } = req.query;
+  try{
+    let allCategories = undefined
+    let product = []
+    if(brand){
+      product = await Product.findAll({
+        where: {
+          [Op.and]: {
+              genre: genre,
+              brandId: brand,
+          }
+        },
+    })}
+    else if (genre){
+      product = await Product.findAll({
+        where:{
+          genre:genre
+        }
+      })
     }
+    else{
+      allCategories = await Category.findAll()
+    }
+
+  
+    let arrayId = []
+    product.forEach(p => {
+      if (arrayId.includes(p.categoryId)){
+        return
+      }
+      else{
+        arrayId.push(p.categoryId)
+      }
+    })
+  
+    if(arrayId.length !== 0){
+      res.status(200).send(await Category.findAll({
+        where: {
+          id: arrayId
+        }
+      }));
+    }
+    else if(allCategories){
+      res.status(200).send(allCategories)
+    }
+    else{
+      res.status(200).send("no hay categorias")
+    }
+  }catch(err){
+    res.status(400).send(err)
   }
 };
 
